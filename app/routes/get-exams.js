8f6bda83e26584ebfb926ba172c52917dfc756bb
Sebastian Wachter <sebi.wachter@online.de>
2017-09-09 18:54:11 +0200

    refactor GET exams

--- app/routes/get-exams.js
+++ app/routes/get-exams.js
@@ -1,12 +1,28 @@
 const Exam = require('../models/exam');
 
 module.exports = (req, res) => {
-  Exam.find({}, (err, docs) => {
-    if (!err) {
-      res.send(docs);
-      process.exit();
-    } else {
+  Exam.find({}, (err, exams) => {
+    if (err) {
       throw (err);
+    } else {
+      findSuccess(exams, req, res);
     }
   });
+};
+
+const findSuccess = (exams, req, res) => {
+  //res.send(exams);
+  const response = [];
+  exams.forEach((exam) => {
+    const customResponseObject = {
+      fileName: exam.concatName,
+      fileType: 'pdf',
+      comments: exam.comments,
+      solutions: exam.solutions,
+      lecturer: exam.lecturer,
+      subject: exam.subject,
+    };
+    response.push(customResponseObject);
+  });
+  res.send(response);
 };
