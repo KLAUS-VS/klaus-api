e0886cab13a6a58783f1df6b51f646a923d5b466
Sebastian Wachter <sebi.wachter@online.de>
2017-09-10 16:43:47 +0200

    change database config to work in the docker setup

--- config/database.js
+++ config/database.js
@@ -1,3 +1,4 @@
 module.exports = {
-  'url': 'mongodb://localhost:27017/klaus-vs',
+  //'url': 'mongodb://localhost:27017/klaus-vs',
+  'url': process.env.DB_PORT ? process.env.DB_PORT.replace('tcp', 'mongodb') + '/klaus-vs' : process.env.MONGODB,
 };
