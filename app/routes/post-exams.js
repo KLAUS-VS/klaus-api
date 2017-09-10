0a049904f48995982f6eda3650a7c4b78ed6dc60
Sebastian Wachter <sebi.wachter@online.de>
2017-09-10 18:50:21 +0200

    remove if else

--- app/routes/post-exams.js
+++ app/routes/post-exams.js
@@ -9,26 +9,25 @@ module.exports = (req, res) => {
       }
     });
     res.send({ status: 'file is not of a valid file type (pdf)' });
-  } else {
-    const newExam = new Exam();
-    newExam.subject = req.body.subject;
-    newExam.subStartWith = getFirstCharacter(removeWhitespace(req.body.subject));
-    newExam.lecturer = req.body.lecturer;
-    newExam.semester = removeWhitespace(req.body.semester);
-    newExam.course = req.body.course;
-    newExam.edv = req.body.edv;
-    newExam.location = req.file.path;
-    newExam.solutions = req.body.solutions;
-    newExam.originalQuestions = req.body.originalQuestions;
-    newExam.comments = req.body.comments;
-    newExam.concatName = removeWhitespace(generateUniqueName(req.body));
-    newExam.save((err) => {
-      if (err) {
-        throw (err);
-      }
-      res.send({ status: 'ok' });
-    });
   }
+  const newExam = new Exam();
+  newExam.subject = req.body.subject;
+  newExam.subStartWith = getFirstCharacter(removeWhitespace(req.body.subject));
+  newExam.lecturer = req.body.lecturer;
+  newExam.semester = removeWhitespace(req.body.semester);
+  newExam.course = req.body.course;
+  newExam.edv = req.body.edv;
+  newExam.location = req.file.path;
+  newExam.solutions = req.body.solutions;
+  newExam.originalQuestions = req.body.originalQuestions;
+  newExam.comments = req.body.comments;
+  newExam.concatName = removeWhitespace(generateUniqueName(req.body));
+  newExam.save((err) => {
+    if (err) {
+      throw (err);
+    }
+    res.send({ status: 'ok' });
+  });
 };
 
 const generateUniqueName = (data) => {
